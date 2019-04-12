c9e54dde9b653f3b76e9c33ea7e450d925c34f6d
Justin Torrance <justin.torrance91@gmail.com>
2019-04-12 09:46:51 -0600

    Make IngredientForm a controlled form

--- src/containers/IngredientsForm/IngredientsForm.js
+++ src/containers/IngredientsForm/IngredientsForm.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react'
 
 export class IngredientsForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      ingredient1: '',
+      ingredient2: '',
+      ingredient3: '',
+      ingredient4: '',
+      ingredient5: '',
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value})
+  }
 
   render() {
+    const { ingredient1, ingredient2, ingredient3, ingredient4, ingredient5 } = this.state
     return(
-      <div>
-        IngredientsForm
-      </div>
+      <form>
+        <input 
+          value={ingredient1}
+          name='ingredient1'
+          onChange={this.handleChange}
+          placeholder='enter an ingredient'
+        />
+        <input 
+          value={ingredient2}
+          name='ingredient2'
+          onChange={this.handleChange}
+          placeholder='enter an ingredient'
+        />
+        <input 
+          value={ingredient3}
+          name='ingredient3'
+          onChange={this.handleChange}
+          placeholder='enter an ingredient'
+        />
+        <input 
+          value={ingredient4}
+          name='ingredient4'
+          onChange={this.handleChange}
+          placeholder='enter an ingredient'
+        />
+        <input 
+          value={ingredient5}
+          name='ingredient5'
+          onChange={this.handleChange}
+          placeholder='enter an ingredient'
+        />
+      </form>
     )
   }
 }
