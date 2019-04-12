1ee22cf37c06047f330c748ea1d2454ade8bb30a
Justin Torrance <justin.torrance91@gmail.com>
2019-04-11 19:07:54 -0600

    Set up index.js with redux, thunks, and router

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+const API_KEY = `${process.env.REACT_APP_API_KEY}`
+const APP_ID = `${process.env.REACT_APP_APP_ID}`
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App
