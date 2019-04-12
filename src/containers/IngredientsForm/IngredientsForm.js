0b0778253c5dab2e8510e978c3b4c76d278479d3
Justin Torrance <justin.torrance91@gmail.com>
2019-04-12 10:19:41 -0600

    Add instructions for user and style IngredientsForm

--- src/containers/IngredientsForm/IngredientsForm.js
+++ src/containers/IngredientsForm/IngredientsForm.js
@@ -20,38 +20,46 @@ export class IngredientsForm extends Component {
   render() {
     const { ingredient1, ingredient2, ingredient3, ingredient4, ingredient5 } = this.state
     return(
-      <form className='ingredients-form'>
-        <input 
-          value={ingredient1}
-          name='ingredient1'
-          onChange={this.handleChange}
-          placeholder='enter an ingredient'
-        />
-        <input 
-          value={ingredient2}
-          name='ingredient2'
-          onChange={this.handleChange}
-          placeholder='enter an ingredient'
-        />
-        <input 
-          value={ingredient3}
-          name='ingredient3'
-          onChange={this.handleChange}
-          placeholder='enter an ingredient'
-        />
-        <input 
-          value={ingredient4}
-          name='ingredient4'
-          onChange={this.handleChange}
-          placeholder='enter an ingredient'
-        />
-        <input 
-          value={ingredient5}
-          name='ingredient5'
-          onChange={this.handleChange}
-          placeholder='enter an ingredient'
-        />
-      </form>
+      <section className='ingredient-input-section'>
+        <div className='instructions'>
+          <p>It's easy to begin!</p>
+          <p>Simply enter the ingredients you have on hand,</p>
+          <p>and we'll find recipes that include all of your ingredients.</p>
+        </div>
+        <form className='ingredients-form'>
+          <input 
+            value={ingredient1}
+            name='ingredient1'
+            onChange={this.handleChange}
+            placeholder='enter an ingredient'
+          />
+          <input 
+            value={ingredient2}
+            name='ingredient2'
+            onChange={this.handleChange}
+            placeholder='enter an ingredient'
+          />
+          <input 
+            value={ingredient3}
+            name='ingredient3'
+            onChange={this.handleChange}
+            placeholder='enter an ingredient'
+          />
+          <input 
+            value={ingredient4}
+            name='ingredient4'
+            onChange={this.handleChange}
+            placeholder='enter an ingredient'
+          />
+          <input 
+            value={ingredient5}
+            name='ingredient5'
+            onChange={this.handleChange}
+            placeholder='enter an ingredient'
+          />
+
+        </form>
+      </section>
     )
   }
 }
