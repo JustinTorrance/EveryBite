bcd043d844ddf5371630cec6d8ad61ffe8eb04de
Justin Torrance <justin.torrance91@gmail.com>
2019-04-11 19:15:25 -0600

    Create IngredientsForm as a class-based component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import IngredientsForm from '../../containers/IngredientsForm/IngredientsForm'
 const API_KEY = `${process.env.REACT_APP_API_KEY}`
 const APP_ID = `${process.env.REACT_APP_APP_ID}`
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <IngredientsForm />
       </div>
     );
   }
