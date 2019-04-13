4c83a9f963830100c64b104e71193d8ecab69763
Justin Torrance <justin.torrance91@gmail.com>
2019-04-12 21:28:51 -0600

    Add time prop to Card and convert to minutes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
-const Card = ({name, id, image, ingredients}) => {
+const Card = ({name, id, image, ingredients, time}) => {
 
  const mappedIngredients = ingredients.map(ingredient => {
    return <p>{ingredient}</p>
  })
 
+ const minutes = parseInt(time) / 60
+
   return(
     <article>
       <h3 className='recipe-name'>{name}</h3>
-      <div className='ingredients-div'>
-        {mappedIngredients}
-      </div>
       <img className='recipe-img' src={image} width='300' />
+      <p>Time: {minutes} minutes</p>
+      <div className='ingredients-div'>
+        Ingredients: {mappedIngredients}
+      </div>
     </article>
   )
 }
