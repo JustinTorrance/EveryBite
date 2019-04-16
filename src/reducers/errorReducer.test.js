7909301c945dfc0919ff4dee847ee593473dfb1c
Justin Torrance <justin.torrance91@gmail.com>
2019-04-16 13:32:14 -0600

    Write tests for errorReducer

---
+++ src/reducers/errorReducer.test.js
@@ -0,0 +1,21 @@
+import { errorReducer } from './errorReducer'
+
+describe('errorReducer', () => {
+
+  it('should return initial state', () => {
+    const expected = ''
+    const result = errorReducer(undefined, '')
+    expect(result).toEqual(expected)
+  })
+
+  it('should return an error message', () => {
+    const expected = 'Something went wrong'
+    const action = {
+      type: 'HAS_ERRORED',
+      message: 'Something went wrong'
+    }
+    const result = errorReducer('', action)
+    expect(result).toEqual(expected)
+  })
+
+})
