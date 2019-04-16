a4585c42010a50c27e91946a342b791e1052a836
Justin Torrance <justin.torrance91@gmail.com>
2019-04-16 12:27:25 -0600

    Test isLoading and hasErrored actions

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -10,4 +10,22 @@ describe('actions', () => {
     const result = actions.storeRecipes(recipes)
     expect(result).toEqual(expected)
   })
+
+  it('should return a type of IS_LOADING and a boolean', () => {
+    const expected = {
+      type: 'IS_LOADING',
+      isLoading: true
+    }
+    const result = actions.isLoading(true)
+    expect(result).toEqual(expected)
+  })
+
+  it('should return a type of HAS_ERRORED and an error message', () => {
+    const expected = {
+      type: 'HAS_ERRORED',
+      message: 'Something went wrong'
+    }
+    const result = actions.hasErrored('Something went wrong')
+    expect(result).toEqual(expected)
+  })
 })
